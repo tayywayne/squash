90f67b78efbe81f3d7b319271eeeed91fa547788
Taylor Wayne <101381253+tayywayne@users.noreply.github.com>
2025-06-25 10:05:29 -0700

    Fix App.tsx Duplicates

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -54,8 +54,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     { name: 'Dashboard', href: '/dashboard', icon: Home, emoji: '🎛️' },
     { name: 'Public Shame', href: '/public-shame', icon: Trophy, emoji: '🧨' },
     { name: 'Quests', href: '/quests', icon: BookOpen, emoji: '📚' },
-    { name: 'Quests', href: '/quests', icon: BookOpen, emoji: '📚' },
-    { name: 'Quests', href: '/quests', icon: Trophy, emoji: '📚' },
     { name: 'Leaderboard',  href: '/leaderboard', icon: Trophy, emoji: '🏆' },
     { name: 'Support Us', href: '/support-us', icon: User, emoji: '🎁' },
     { name: 'Reddit Drama', href: '/reddit-conflict', icon: MessageSquare, emoji: '👓' },
